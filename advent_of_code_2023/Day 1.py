192c2bedc0baa00114c83c0582ee9b244fdfcd42
Gibson Fahnestock <gibfahn@gmail.com>
2023-12-30 14:00:44 +0800

    Advent of code day one part two gooder solution
    Less typing, more looping

--- advent_of_code_2023/Day 1.py
+++ advent_of_code_2023/Day 1.py
@@ -1,78 +1,45 @@
+numbers = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
+    "zero": "0",
+}
+
+def find_first_number(line: str) -> str:
+    for position in range(len(line)):
+        if line[position].isnumeric():
+            return line[position]
+
+        for x in numbers:
+            if line[position:].startswith(x):
+                return numbers[x]
+
+def find_last_number(line: str) -> str:
+    for position in range(len(line) - 1, -1, -1):
+        if line[position].isnumeric():
+            return line[position]
+        for x in numbers:
+            reversed_x = x[::-1]
+            if line[position:0:-1].startswith(reversed_x):
+                return numbers[x]
+        
+
+
 f = open("./day-01-input", "r")
 input = f.read()
 total = 0
-for line in input.splitlines():
-    for position in range(len(line)):
-        if line[position].isnumeric():
-            num1 = line[position]
-            break
-        if line[position:].startswith("one"):
-            num1 = "1"
-            break
-        if line[position:].startswith("two"):
-            num1 = "2"
-            break
-        if line[position:].startswith("three"):
-            num1 = "3"
-            break
-        if line[position:].startswith("four"):
-            num1 = "4"
-            break
-        if line[position:].startswith("five"):
-            num1 = "5"
-            break
-        if line[position:].startswith("six"):
-            num1 = "6"
-            break
-        if line[position:].startswith("seven"):
-            num1 = "7"
-            break
-        if line[position:].startswith("eight"):
-            num1 = "8"
-            break
-        if line[position:].startswith("nine"):
-            num1 = "9"
-            break
-        if line[position:].startswith("zero"):
-            num1 = "0"
-            break
 
-    for position in range(len(line) - 1, -1, -1):
-        if line[position].isnumeric():
-            num_last = line[position]
-            break
-        if line[position:0:-1].startswith("eno"):
-            num_last = "1"
-            break
-        if line[position:0:-1].startswith("owt"):
-            num_last = "2"
-            break
-        if line[position:0:-1].startswith("eerht"):
-            num_last = "3"
-            break
-        if line[position:0:-1].startswith("ruof"):
-            num_last = "4"
-            break
-        if line[position:0:-1].startswith("evif"):
-            num_last = "5"
-            break
-        if line[position:0:-1].startswith("xis"):
-            num_last = "6"
-            break
-        if line[position:0:-1].startswith("neves"):
-            num_last = "7"
-            break
-        if line[position:0:-1].startswith("thgie"):
-            num_last = "8"
-            break
-        if line[position:0:-1].startswith("enin"):
-            num_last = "9"
-            break
-        if line[position:0:-1].startswith("orez"):
-            num_last = "0"
-            break
+for line in input.splitlines():
+    num1 = find_first_number(line)
+    num_last = find_last_number(line)
+
     digit = num1 + num_last
     print(digit)
     total += int(digit)
 print(total)
-
