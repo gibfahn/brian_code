d619cb9f1920192bb232bd599bb36262f14398cc
Gibson Fahnestock <gibfahn@gmail.com>
2023-12-30 13:13:33 +0800

    Advent of code day one part two working solution
    Very painful and manual solution, but it works.

--- advent_of_code_2023/Day 1.py
+++ advent_of_code_2023/Day 1.py
@@ -1,16 +1,78 @@
-f = open("./day-01-input.txt", "r")
+f = open("./day-01-input", "r")
 input = f.read()
 total = 0
 for line in input.splitlines():
-    for char in line:
-        if char.isnumeric():
-            num1 = char
+    for position in range(len(line)):
+        if line[position].isnumeric():
+            num1 = line[position]
+            break
+        if line[position:].startswith("one"):
+            num1 = "1"
+            break
+        if line[position:].startswith("two"):
+            num1 = "2"
+            break
+        if line[position:].startswith("three"):
+            num1 = "3"
+            break
+        if line[position:].startswith("four"):
+            num1 = "4"
+            break
+        if line[position:].startswith("five"):
+            num1 = "5"
+            break
+        if line[position:].startswith("six"):
+            num1 = "6"
+            break
+        if line[position:].startswith("seven"):
+            num1 = "7"
+            break
+        if line[position:].startswith("eight"):
+            num1 = "8"
+            break
+        if line[position:].startswith("nine"):
+            num1 = "9"
+            break
+        if line[position:].startswith("zero"):
+            num1 = "0"
             break
 
-    for char in line[::-1]:
-        if char.isnumeric():
-            num_last = char
+    for position in range(len(line) - 1, -1, -1):
+        if line[position].isnumeric():
+            num_last = line[position]
+            break
+        if line[position:0:-1].startswith("eno"):
+            num_last = "1"
+            break
+        if line[position:0:-1].startswith("owt"):
+            num_last = "2"
+            break
+        if line[position:0:-1].startswith("eerht"):
+            num_last = "3"
+            break
+        if line[position:0:-1].startswith("ruof"):
+            num_last = "4"
+            break
+        if line[position:0:-1].startswith("evif"):
+            num_last = "5"
+            break
+        if line[position:0:-1].startswith("xis"):
+            num_last = "6"
+            break
+        if line[position:0:-1].startswith("neves"):
+            num_last = "7"
+            break
+        if line[position:0:-1].startswith("thgie"):
+            num_last = "8"
+            break
+        if line[position:0:-1].startswith("enin"):
+            num_last = "9"
+            break
+        if line[position:0:-1].startswith("orez"):
+            num_last = "0"
             break
     digit = num1 + num_last
+    print(digit)
     total += int(digit)
 print(total)
+
