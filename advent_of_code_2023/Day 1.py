6aa9757c144a8c84d1bde32160b0ee00cec00caf
Gibson Fahnestock <gibfahn@gmail.com>
2023-12-30 12:13:58 +0800

    Advent of code day 1 part 1 working solution

--- advent_of_code_2023/Day 1.py
+++ advent_of_code_2023/Day 1.py
@@ -1,4 +1,16 @@
-f = open("day-01-input", "r")
+f = open("./day-01-input.txt", "r")
 input = f.read()
+total = 0
 for line in input.splitlines():
-    print(line)
+    for char in line:
+        if char.isnumeric():
+            num1 = char
+            break
+
+    for char in line[::-1]:
+        if char.isnumeric():
+            num_last = char
+            break
+    digit = num1 + num_last
+    total += int(digit)
+print(total)
