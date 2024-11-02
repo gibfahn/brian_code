a78021d90b6cffa5ec82923d295e0fac79f940d6
Gibson Fahnestock <gibfahn@gmail.com>
2024-11-02 12:40:16 +0000

    feat(gcse): add initial question

---
+++ gcse_prep/staff_names_and_their_phone_numbers/t.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+StaffName = ["Janice West", "Frank Knight", "Elsa Alsace-Lorraine"]
+
+StaffPhone = ["01923489154", "01942865292", "07752032491"]
+
+StaffOffice = ["London", "Tokyo", "Peru"]
