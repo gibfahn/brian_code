87587f4c6f20e9dbf810cb801474b7d7c37faf89
Gibson Fahnestock <gibfahn@gmail.com>
2024-11-05 15:25:57 +0000

    Add sample questions

--- gcse_prep/school_lunches/t.py
+++ gcse_prep/school_lunches/t.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 Menu = ["chips", "soss", "egg", "hoisin crispy owl"]
 Price = [1.23, 2.50, 3.00, 28.12]
--- gcse_prep/staff_names_and_their_phone_numbers/t.py
+++ gcse_prep/staff_names_and_their_phone_numbers/t.py
@@ -1,9 +1,34 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-StaffName = ["Janice West", "Frank Knight", "Elsa Alsace-Lorraine"]
+StaffName = ["Janice West", "Frank Knight", "Elsa Alsace-Lorraine", "Robert Bobert"]
 
-StaffPhone = ["01923489154", "01942865292", "07752032491"]
+StaffPhone = ["01923489154", "01942865292", "07752032491", "07654321098"]
 
-StaffOffice = ["London", "Tokyo", "Peru"]
+StaffOffice = ["London", "Tokyo", "Peru", "New York"]
 
-print("Yo")
+def bubble_sort(arr: list):
+  swaps = True
+  while swaps:
+    swaps = False
+    for i in range(0, len(arr) - 1):
+      if arr[i][0] > arr[i+1][0]:
+        arr[i], arr[i+1] = arr[i+1], arr[i]
+        swaps = True
+
+def display_staff():
+  name_and_phone = []
+  for i in range(0, len(StaffName)):
+      name_and_phone += [[StaffName[i], StaffPhone[i]]]
+  bubble_sort(name_and_phone)
+  print(name_and_phone)
+
+display_staff()
+
+"""
+arr = [[]]
+for i in range(0, len(StaffName)):
+    arr.append([StaffName[i]])
+print(arr)
+for r in range(0, len(StaffPhone)):
+    a =0
+"""
